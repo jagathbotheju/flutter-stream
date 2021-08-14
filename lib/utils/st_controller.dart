6ad85ea80eb767d3e9d5321947b75f7e781e0b92
jagathbotheju <jagathbotheju@gmail.com>
2021-08-15 01:14:18 +0300

    second-commit

--- lib/utils/st_controller.dart
+++ lib/utils/st_controller.dart
@@ -1,19 +1,31 @@
 import 'dart:async';
 
-class StController {
-  final StreamController<int> _numbersController =
-      StreamController<int>.broadcast();
+import 'package:flutter/material.dart';
+
+class StController extends StatefulWidget {
+  @override
+  _StControllerState createState() => _StControllerState();
+}
+
+class _StControllerState extends State<StController> {
+  late final StreamController<int> _numbersController;
   Stream<int> get outNumbers => _numbersController.stream;
   Sink<int> get inNumbers => _numbersController.sink;
 
   @override
-  void dispose() { 
-    _
+  void initState() {
+    super.initState();
+    _numbersController = StreamController<int>.broadcast();
+  }
+
+  @override
+  void dispose() {
+    _numbersController.close();
     super.dispose();
   }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
 }
-
-
-
-
-7:26
